e0b939d304cf1ec3df3ea29013d5218404d044cf
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2016-12-07 19:38:44 +0900

    initial commit

---
+++ decision_tree_for_hems_recommendations/app.py
@@ -0,0 +1,42 @@
+# coding: utf-8
+
+'''
+1. [] Building Decision Tree (DT) Model from past Tenki and HEMS data
+  1. [] Make Decision Table
+  2. [] Make Decision Tree
+
+2. Decide to deliver the contents using the DT Model and the today's Tenki data
+  1. [] Get the today's Tenki data
+  1. [] Give the decision flags
+'''
+
+
+class RecommnedationDecisionTree:
+    def make_decision_table(self):
+        '''
+        X_data
+        [
+          [],
+          [],
+          [],
+          [],
+        ]
+
+        Y_data
+        [
+          0,
+          0,
+          0,
+          0,
+        ]
+        '''
+        pass
+
+    def make_decision_tree(self):
+        pass
+
+    def get_the_predicted_tenki_data(self):
+        pass
+
+    def ret_the_decision_flags(self):
+        pass
