1ec40f249848a90e58f6ce38239edfacc2f1c632
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2016-12-14 16:42:28 +0900

    fix version to 0.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="decision_tree_for_hems_recommendations",
-    version="0.0.2",
+    version="0.0.3",
     author="Shintaro Ikeda",
     author_email="ikenshirogivenup98@gmail.com",
     license="MIT",
